c7ea78d55019d0e132799a980443eacb37150de0
SnowmanSeniorDev <snowman970919@gmail.com>
2021-10-15 10:39:40 +0800

    admin user update

--- src/views/users/Edit.vue
+++ src/views/users/Edit.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="post intro-y overflow-hidden box mt-5">
-    <div
-      class="post__tabs nav nav-tabs flex-col sm:flex-row bg-gray-300 text-gray-600"
-      role="tablist"
-    >
+    <div class="post__tabs nav nav-tabs flex-col sm:flex-row bg-gray-300 text-gray-600" role="tablist">
       <a
         id="user-tab"
         data-toggle="tab"
@@ -240,108 +237,49 @@
     </div>
     <div id="bank-info" class="tab-pane p-5" :class="`${panel === 'bank-info' ? 'block' : 'hidden'}`">
       <div class="border border-gray-200 rounded-md p-5">
-        <div class="font-medium flex items-center grid grid-cols-12">
-          <div v-for="(item, index) in bankInfos" :key="index" class="col-span-12 md:col-span-6 intro-y">
-            <p>Bank Name : {{bankInfos[index].bankName}}</p>
-            <p>Bank Branch Name : {{bankInfos[index].branchName}}</p>
-            <p>Bank Account Number : {{bankInfos[index].address}}</p>
-            <p>Bank Swift Code : {{bankInfos[index].swiftCode}}</p>
-            <p>Bank Currency : {{bankInfos[index].currency}}</p>
-          </div>
-        </div>
+        <userBankInfo v-if="companyBankInforReady" :bankInfo="bankInfo" :companyId="user_company_uuid" />
       </div>
     </div>
     <div id="currency-settings" class="tab-pane p-5" :class="`${panel === 'currency-settings' ? 'block' : 'hidden'}`">
       <div class="border border-gray-200 rounded-md p-5">
-        <div class="font-medium flex items-center border-b border-gray-200 pb-5">
-            <table class="table table--lg">
-              <thead>
-                <tr class="bg-theme-2">
-                  <th class="whitespace-nowrap"> CURRENCY CODE </th>
-                  <th class="whitespace-nowrap"> CURRENCY NAME </th>
-                  <th class="whitespace-nowrap"> CURRENCY SYMBOL </th>
-                  <th class="whitespace-nowrap"> SUPPORT </th>
-                  <th class="whitespace-nowrap"> DEFAULT </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="odd:bg-gray-200" v-for="(currency, index) in currencies" :key="index">
-                  <td class="">{{currency.currencyCode}}</td>
-                  <td class="">{{currency.currencyName}}</td>
-                  <td class="">{{currency.currencySymbol}}</td>
-                  <td class="">
-                    <input id="input-wizard-4-currency-eur"
-                      v-model="support"
-                      type="checkbox"
-                      class="form-check-input"
-                      :value="currency.currencyCode" disabled/>
-                  </td>
-                  <td class="">
-                    <input id="input-wizard-4-currency-eur-default"
-                      v-model="defaultCurrency"
-                      type="radio"
-                      class="form-check-input"
-                      name="default-currency"
-                      :value="currency.currencyCode" readonly/>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-        </div>
+        <userCurrencySetting :companyId="user_company_uuid" />
       </div>
     </div>
     <div id="kyc-document" class="tab-pane p-5" :class="`${panel === 'kyc-document' ? 'block' : 'hidden'}`">
-      <div class="border border-gray-200 rounded-md p-5">
-        <div class="font-medium flex items-center border-b border-gray-200 pb-5">
-          <table class="table table--lg">
-            <thead>
-              <tr class="bg-theme-2">
-                <th class="whitespace-nowrap w-1/5"> DOCUMENT CATEGORY </th>
-                <th class="whitespace-nowrap w-3/5"> STATUS </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="odd:bg-gray-200" v-for="(item, index) in docList" :key="index">
-                <td class="">{{item.category}}</td>
-                <td class="">
-                  <div class="alert alert-warning-soft show flex items-center justify-center h-5 p-3 text-sm" role="alert">
-                    {{item.status}}
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <userKycDocuments :companyId="user_company_uuid" />
     </div>
   </div>
 </template>
-j
+
 <script>
-import { reactive, toRefs, ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-import { sysAxios, appAxios } from "@/plugins/axios";
-import { required, minLength, email } from "@vuelidate/validators";
-import { useVuelidate } from "@vuelidate/core";
-import Toastify from "toastify-js";
-import userComapnyProfile from './UserCompanyProfile.vue'
+import { reactive, toRefs, ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { sysAxios, appAxios } from '@/plugins/axios'
+import { required, minLength, email } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
+import Toastify from 'toastify-js'
+import userComapnyProfile from './templates/UserCompanyProfile.vue'
+import userBankInfo from './templates/UserCompanyProfile.vue'
+import userCurrencySetting from './templates/UserCurrencySetting.vue'
+import userKycDocuments from './templates/UserKycDocuments.vue'
 
 export default {
   components: {
-    userComapnyProfile
+    userComapnyProfile, userBankInfo, userCurrencySetting, userKycDocuments
   },
   setup() {
-    const route = useRoute();
-    const roles = ref([]);
-    const selectedRoles = ref([]);
-    const authorizationId = ref('');
+    const route = useRoute()
+    const roles = ref([])
+    const selectedRoles = ref([])
+    const authorizationId = ref('')
     const panel = ref('user')
     const formData = reactive({
       firstName: "",
       lastName: "",
       email: "",
       displayName: "",
-    });
+    })
+    const user_company_uuid = ref('')
     const companyProfile = ref({
 			companyDisplayName: '',
 			companyLegalName: '',
@@ -356,8 +294,8 @@ export default {
 			country: null,
 			phone: '',
 			primaryEmail: '',
-		});
-    const bankInfos = ref([
+		})
+    const bankInfo = ref([
 			{
 				bankName: '',
 				branchName: '',
@@ -366,18 +304,20 @@ export default {
 				swiftCode: '',
 				currency: '',
 			},
-		]);
-    const currencies = ref(null);
-    const defaultCurrency = ref(null);
-    const support = ref([]);
-    const docList = ref([]);
+		])
+    const currencies = ref(null)
+    const defaultCurrency = ref(null)
+    const support = ref([])
+    const docList = ref([])
     const companyProfileReady = ref(false)
+    const companyBankInforReady = ref(false)
     const pageLoading = ref(true)
-    
+
+
     const getCompanyProfile = async company_uuid => {
-      const getCompanyInfo = `/company/v1/${company_uuid}`;
-      const res = await appAxios.get(getCompanyInfo);
-      companyProfile.value = {...res.data};
+      const getCompanyInfo = `/company/v1/${company_uuid}`
+      const res = await appAxios.get(getCompanyInfo)
+      companyProfile.value = {...res.data}
       companyProfileReady.value = true
       return new Promise(resolve => {
         resolve(res.data)
@@ -385,104 +325,29 @@ export default {
     }
 
     const getBankInfo = async company_uuid => {
-      const getBankInfo = `/company/v1/${company_uuid}/bankaccounts`;
+      const getBankInfo = `/company/v1/${company_uuid}/bankaccounts`
       await appAxios.get(getBankInfo).then(res => {
 				if(res.data.length !== 0) {
-					bankInfos.value = [...res.data];
+					bankInfo.value = [...res.data]
 				}
 			})
+      companyBankInforReady.value = true
       return new Promise(resolve => {
         resolve('backInfo')
       })
     }
-
-    const getCompanyCurrency = async company_uuid => {
-      const companyProfileSystemConfig = 'configuration/v1/Company Profile';
-      const getCompanyCurrencies = `/company/v1/${company_uuid}/currencies`;
-      await sysAxios.get(companyProfileSystemConfig).then(res => {
-				currencies.value = JSON.parse(_.find(res.data[0].configurations, {name: "currencies"}).value)
-			})
-      await appAxios.get(getCompanyCurrencies).then(res => {
-        if(res.data.length) {
-          res.data.forEach(item => {
-            if(item.isDefault) defaultCurrency.value = item.currencyCode;
-            support.value.push(item.currencyCode);
-          });
-          currencies.value = {...res.data, ...currencies.value}
-        }
-      })
-      return new Promise(resolve => {
-        resolve('get company currency')
-      })
-    }
-
-    const getKycDocuments = async company_uuid => {
-      const companyProfileSystemConfig = 'configuration/v1/Company Profile';
-      const getCompanyCoporateInfoApi = `/company/v1/${company_uuid}/corporateinfo`;
-      await sysAxios.get(companyProfileSystemConfig).then(res => {
-        JSON.parse(_.find(res.data[0].configurations, {name: "kyc_dument_category"}).value).forEach(item => {
-          docList.value.push({
-            corporateInfoHeaderId: '',
-            category: item,
-            status: 'waiting for document upload',
-          })
-        })
-      })
-      await appAxios.get(getCompanyCoporateInfoApi).then(res => {
-        for(let index in res.data){
-          docList.value[_.findIndex(docList.value, {category: res.data[index].corporateInfoDocumentCategoryName})] = {
-            corporateInfoHeaderId: res.data[index].corporateInfoHeaderId,
-            category: res.data[index].corporateInfoDocumentCategoryName,
-            status: res.data[index].approvalStatus
-          }
-        }
-      })
-    }
-
-    onMounted(async () => {
-      const authorizationApi = `access/v1/authorization/user/${route.params.id}`;
-      sysAxios.get(authorizationApi).then(res => {
-        authorizationId.value = res.data.authorizationId
-        res.data.roles.forEach(role => {
-          selectedRoles.value.push(role.roleId);
-        });
-
-        const rolesApi = "access/v1/role";
-        sysAxios.get(rolesApi).then(res => {roles.value = res.data});
-      })
-
-      const userApi = "/user/v1/" + route.params.id;
-      sysAxios.get(userApi).then((res) => {
-        formData.firstName = res.data.firstName,
-        formData.lastName = res.data.lastName,
-        formData.displayName = res.data.displayName,
-        formData.email = res.data.emailAddress
-      })
-      const company_uuid = await appAxios.get(`company/v1/user/${route.params.id}`).then(res => {
-        if(res.status === 'error') return '00000000-0000-0000-0000-000000000000';
-        else return res.data
-      })
-      if(company_uuid !== '00000000-0000-0000-0000-000000000000') {
-        console.log(company_uuid)
-        await getCompanyProfile(company_uuid)
-        await getBankInfo(company_uuid)
-        await getCompanyCurrency(company_uuid)
-        await getKycDocuments(company_uuid)
-      }
-      pageLoading.value = false
-    })
-
-
+    
     const rules = {
       firstName: { required, minLength: minLength(2) },
       lastName: { required, minLength: minLength(2)},
       email: { required, email },
       displayName: { required, minLength: minLength(6)},
-    };
+    }
 
-    const validate = useVuelidate(rules, toRefs(formData));
+    const validate = useVuelidate(rules, toRefs(formData))
+    
     const saveUserData = () => {
-      validate.value.$touch();
+      validate.value.$touch()
       if (validate.value.$invalid) {
         Toastify({
           node: cash("#failed-notification-content").clone().removeClass("hidden")[0],
@@ -492,9 +357,9 @@ export default {
           gravity: "top",
           position: "right",
           stopOnFocus: true
-        }).showToast();
+        }).showToast()
       } else {
-        const api = `/user/v1/${route.params.id}`;
+        const api = `/user/v1/${route.params.id}`
         sysAxios.put(api, {
           emailAddress: formData.email,
           firstName: formData.firstName,
@@ -502,7 +367,7 @@ export default {
           displayName: formData.displayName
         }).then(res => {
           if(res.status === 200){
-            const api = `access/v1/authorization/${authorizationId.value}`;
+            const api = `access/v1/authorization/${authorizationId.value}`
             sysAxios.put(api, {applicationDomain: "genie", roleIds: selectedRoles.value}).then(res => {
               if(res.status === 200){
                 Toastify({
@@ -513,14 +378,47 @@ export default {
                   gravity: "top",
                   position: "right",
                   stopOnFocus: true
-                }).showToast();
+                }).showToast()
               }
             })
             
           }
         })
       }
-    };
+    }
+
+    onMounted(async () => {
+      const authorizationApi = `access/v1/authorization/user/${route.params.id}`
+      sysAxios.get(authorizationApi).then(res => {
+        authorizationId.value = res.data.authorizationId
+        res.data.roles.forEach(role => {
+          selectedRoles.value.push(role.roleId)
+        })
+
+        const rolesApi = "access/v1/role"
+        sysAxios.get(rolesApi).then(res => {roles.value = res.data})
+      })
+
+      const userApi = "/user/v1/" + route.params.id
+      sysAxios.get(userApi).then((res) => {
+        formData.firstName = res.data.firstName,
+        formData.lastName = res.data.lastName,
+        formData.displayName = res.data.displayName,
+        formData.email = res.data.emailAddress
+      })
+      const company_uuid = await appAxios.get(`company/v1/user/${route.params.id}`).then(res => {
+        if(res.status === 'error') return '00000000-0000-0000-0000-000000000000'
+        else return res.data
+      })
+      if(company_uuid !== '00000000-0000-0000-0000-000000000000') {
+        console.log(company_uuid)
+        user_company_uuid.value = company_uuid
+        await getCompanyProfile(company_uuid)
+        await getBankInfo(company_uuid)
+      }
+      pageLoading.value = false
+    })
+
 
     return {
       validate,
@@ -528,16 +426,18 @@ export default {
       saveUserData,
       selectedRoles,
       roles,
+      user_company_uuid,
       companyProfile,
       pageLoading,
       companyProfileReady,
-      bankInfos,
+      companyBankInforReady,
+      bankInfo,
       currencies,
       defaultCurrency,
       support,
       docList,
-      panel
-    };
+      panel,
+    }
   }
-};
+}
 </script>
--- src/views/users/templates/UserBankInfo.vue
+++ src/views/users/templates/UserBankInfo.vue
@@ -1,6 +1,5 @@
 <template>
 	<div class="">
-		<div class="font-medium text-2xl">Bank Information</div>
 		<div v-for="(item, index) in bankInfos" :key="index" class="intro-y grid grid-cols-2 gap-4 border-b border-gray-500 py-4">
 			<div class="">
 				<div class="flex justify-between">
@@ -55,56 +54,50 @@
 			</div>
 		</div>
 		<div id="failed-notification-content" class="toastify-content hidden flex">
-				<XCircleIcon class="text-theme-6" />
-				<div class="ml-4 mr-4">
-					<div class="font-medium">Add Bank Info was failed!</div>
-					<div class="text-gray-600 mt-1">Please fillin correct value of the company profile.</div>
-				</div>
+      <XCircleIcon class="text-theme-6" />
+      <div class="ml-4 mr-4">
+        <div class="font-medium">Add Bank Info was failed!</div>
+        <div class="text-gray-600 mt-1">Please fillin correct value of the company profile.</div>
+      </div>
 		</div>
 	</div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
-import { ref, onMounted } from "vue";
-import { sysAxios, appAxios } from "@/plugins/axios";
-import Toastify from "toastify-js";
-import _ from "lodash";
+import { ref, onMounted } from 'vue'
+import { sysAxios, appAxios } from '@/plugins/axios'
+import Toastify from 'toastify-js'
+import _ from 'lodash'
 export default {
-	setup() {
-		const router = useRouter();
-    const store = useStore();
-		const banks = ref([]);
-		const currencies = ref([]);
-		const bankInfos = ref([
-			{
-				bankName: '',
-				branchName: '',
-				address: '',
-				accountNumber: '',
-				swiftCode: '',
-				currency: '',
-			},
-		]);
-		const deletedBank = ref([]);
-		const originBankInfo = ref([]);
+  props: {
+    bankInfo: {
+      type: Object,
+      required: true,
+    },
+    companyId: {
+      type: String,
+      required: true
+    }
+  },
+	setup(props) {
+		const banks = ref([])
+		const currencies = ref([])
+		const bankInfos = ref([...props.bankInfo])
+		const deletedBank = ref([])
+		const originBankInfo = ref([])
+    const company_uuid = props.companyId
 
 		onMounted(async () => {
-			const companyProfileSystemConfig = 'configuration/v1/Company Profile';
-			const getAccountBankInfo = `/company/v1/${store.state.account.company_uuid}/bankaccounts`;
+			const companyProfileSystemConfig = 'configuration/v1/Company Profile'
 			await sysAxios.get(companyProfileSystemConfig).then(res => {
-				banks.value = JSON.parse(_.find(res.data[0].configurations, {name: "banks"}).value);
-				currencies.value = JSON.parse(_.find(res.data[0].configurations, {name: "currencies"}).value);
+				banks.value = JSON.parse(_.find(res.data[0].configurations, {name: "banks"}).value)
+				currencies.value = JSON.parse(_.find(res.data[0].configurations, {name: "currencies"}).value)
 			})
-			await appAxios.get(getAccountBankInfo).then(res => {
-				if(res.data.length !== 0) {
-					bankInfos.value = [...res.data];
-					res.data.forEach((item) => {
-						originBankInfo.value.push({...item})
-					})
-				}
-			})
+      if(bankInfos.value.length !== 0) {
+        bankInfos.value.forEach((item) => {
+          originBankInfo.value.push({...item})
+        })
+      }
 		})
 
 		const addBank = () => {
@@ -119,7 +112,7 @@ export default {
 		}
 
 		const removeBank = (index, item) => {
-			if(item.bankAccountId) deletedBank.value.push(item.bankAccountId);
+			if(item.bankAccountId) deletedBank.value.push(item.bankAccountId)
 			_.pullAt(bankInfos.value, [index])
 		}
 
@@ -132,96 +125,69 @@ export default {
 				gravity: "top",
 				position: "right",
 				stopOnFocus: true
-			}).showToast();
+			}).showToast()
 		}
 		
 		const registerBankRequest = async (banks) => {
-			const registerBankApiUrl = `/company/v1/${store.state.account.company_uuid}/bankaccount`;
-			const res = await appAxios.post(registerBankApiUrl, [...banks]);
-			if(res.status === 201) return {result: true};
-			return {result: false, response: res.data};
+			const registerBankApiUrl = `/company/v1/${company_uuid}/bankaccount`
+			const res = await appAxios.post(registerBankApiUrl, [...banks])
+			if(res.status === 201) return {result: true}
+			return {result: false, response: res.data}
 		}
 
 		const updateBankRequest = async (bank) => {
 			console.log(bank)
-			const updateBankApiUrl = `/company/v1/${store.state.account.company_uuid}/bankaccount/${bank.bankAccountId}`;
-			delete bank['bankAccountId'];
+			const updateBankApiUrl = `/company/v1/${company_uuid}/bankaccount/${bank.bankAccountId}`
+			delete bank['bankAccountId']
 			console.log(bank)
-			const res = await appAxios.put(updateBankApiUrl, bank);
-			if(res.status === 200) return {result: true};
-			return {result: false, response: res.data};
+			const res = await appAxios.put(updateBankApiUrl, bank)
+			if(res.status === 200) return {result: true}
+			return {result: false, response: res.data}
 		}
 
 		const deleteBankRequest = async (bankId) => {
-			const deleteBankApiUrl = `/company/v1/${store.state.account.company_uuid}/bankaccount/${bankId}`;
-			const res = await appAxios.delete(deleteBankApiUrl);
-			if(res.status === 200 || res.status === 204) return {result: true};
-			return {result: false, response: res.data};
+			const deleteBankApiUrl = `/company/v1/${company_uuid}/bankaccount/${bankId}`
+			const res = await appAxios.delete(deleteBankApiUrl)
+			if(res.status === 200 || res.status === 204) return {result: true}
+			return {result: false, response: res.data}
 		}
 
     const submitBanks = async () => {
 			if(originBankInfo.value.length === 0) {
 				const status = await registerBankRequest(bankInfos.value)
-				if(status.result) {
-					showNotification(true)
-					gotoNext();
-				} else {
-					showNotification(false)
-					console.log(status.response);
-				}
+				if(status.result) showNotification(true)
+        else showNotification(false)
 			} else {
 				for (const bankId of deletedBank.value) {
-					console.log(bankId)
 					var deleteRes = await deleteBankRequest(bankId)
 					if(!deleteRes.result) {
-						showNotification(false);
-						return;
+						showNotification(false)
+						return
 					}
 				}
-				// await deletedBank.value.forEach(async bankId => {
-				// 	var res = await deleteBankRequest(bankId)
-				// 	console.log("delete bank", res)
-				// 	if(!res.result) {
-				// 		showNotification(false);
-				// 		return;
-				// 	}
-				// });
 				
-				var res = await registerBankRequest(_.filter(bankInfos.value, (item) => {if(!item.bankAccountId) return item}));
+				var res = await registerBankRequest(_.filter(bankInfos.value, (item) => {if(!item.bankAccountId) return item}))
 				if(!res.result) {
-					showNotification(false);
-					return;
+					showNotification(false)
+					return
 				}
 				_.filter(bankInfos.value, async (item) => {
 					if(item.bankAccountId) {
 						if (JSON.stringify(_.find(originBankInfo.value, {bankAccountId: item.bankAccountId})) !== JSON.stringify(item)){
-							var res = await updateBankRequest(item);
+							var res = await updateBankRequest(item)
 							if(!res.result) {
-								showNotification(false);
-								return;
+								showNotification(false)
+								return
 							}
 						}
 					}
 				})
-				showNotification(true);
-				gotoNext();
+				showNotification(true)
 			}
 			
     }
 
-    const gotoBack = () => {
-      store.commit('account/SET_STEP', {step: "company-information"});
-      router.go(-1);
-    }
-
-		const gotoNext = () => {
-			store.commit('account/SET_STEP', {step: "currency-settings"});
-			router.push({path: "/account/currency-settings"});
-		}
-
     return {
-      gotoNext,
-      gotoBack,
 			bankInfos,
 			banks,
 			currencies,
